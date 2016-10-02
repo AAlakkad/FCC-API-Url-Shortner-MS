113d31aa52844c33b40482919249a16edc64ec90
Ammar Alakkad <ammar@Ammars-MacBook-Pro.local>
2016-10-02 11:09:56 +0300

    Update landing page /

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(function(req, res, next) {
 });
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.send('Please refer to the docs: https://github.com/AAlakkad/FCC-API-Url-Shortner-MS');
 });
 
 app.get('/new', function(req, res) {
